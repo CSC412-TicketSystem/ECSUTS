bfa11e09863b033d20702e8e8a14567896103e65
Jefferson Ridgeway <jeffersonridgeway@Jeffersons-MacBook-Pro.local>
2016-10-24 10:59:03 -0400

    Commit_1

---
+++ OMTS/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+#Two different tables:
+# 1) student_info => will hold information such as (first & last name, residench hall, issues, etc.)
+# 2) student_ticket => will create a student ticket number
+
+#class studentInfo(models.Model):
+   # first_name = models.CharField(max_length=50)
+   # last_name = models.CharField(max_length=150)
+   # resid_hall = models.TextField()
+   # room_num = models.
