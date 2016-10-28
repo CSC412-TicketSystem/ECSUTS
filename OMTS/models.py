528d3ca68637b9464fcd612991c798376623ca72
Jefferson Ridgeway <jeffersonridgeway@Jeffersons-MacBook-Pro.local>
2016-10-28 10:42:10 -0400

    added middleware back to settings.py

--- OMTS/models.py
+++ OMTS/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 #Two different tables:
-# 1) student_info => will hold information such as (first & last name, residench hall, issues, etc.)
+# 1) student_info => will hold information such as (first & last name, residence hall, issues, etc.)
 # 2) student_ticket => will create a student ticket number
 
 class studentInfo(models.Model):
