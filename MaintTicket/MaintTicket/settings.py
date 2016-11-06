65a5cfaa36ea110dfe45d4596ef7dbf604548eb3
Daniel <deodell100@students.ecsu.edu>
2016-11-06 16:46:22 -0500

    Updated Pages

--- MaintTicket/MaintTicket/settings.py
+++ MaintTicket/MaintTicket/settings.py
@@ -16,6 +16,7 @@ PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
 PROJECT_PATH = os.path.abspath(PROJECT_PATH)
 TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
