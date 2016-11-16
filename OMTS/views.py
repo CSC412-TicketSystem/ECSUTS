60432fb1cd5de04a140a21d6d43742ac40482867
Jefferson Ridgeway <jeffersonridgeway@Jeffersons-MacBook-Pro.local>
2016-11-16 14:29:53 -0700

    trying to figure out how to fix the view

--- OMTS/views.py
+++ OMTS/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import studentInfo
 #from .models import studentInfo, studentTicket
 
 # Create your views here.
 
 def index(request):
+    
+    
     template = loader.get_template('OMTS/test.html')
     return HttpResponse(template.render(request))
+ 
+
+def begform(request):
+    template = loader.get_template('OMTS/')
