b01ba39bbc106b54680de1cb1d4b18b2bb1f84a9
Jefferson Ridgeway <jeffersonridgeway@Jeffersons-MacBook-Pro.local>
2016-11-16 09:56:01 -0700

    added urls patterns and views

--- OMTS/views.py
+++ OMTS/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+#from .models import studentInfo, studentTicket
 
 # Create your views here.
+
+def index(request):
+    template = loader.get_template('OMTS/test.html')
+    return HttpResponse(template.render(request))
--- project1/urls.py
+++ project1/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from OMTS import views
+
+
+app_name = 'OMTS'
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
 ]
